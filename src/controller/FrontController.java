ad25c0a42fbf68053ed776e5c02fb1b693a2dbe8
hyunkim <han.kim@kakao.com>
2021-06-16 17:44:17 +0900

    [fix] : login jsp url 매핑

--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -34,7 +34,7 @@ public class FrontController extends HttpServlet{
 		String com = uri.substring(conPath.length());
 
 		if (com.equals("/")) {
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/view/login.jsp");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/view/main.jsp");
 			dispatcher.forward(request, response);
 
 		} else {
