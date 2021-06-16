07605a74f9e9ee319b9fc65629b3fa33df95dd64
hyunkim <han.kim@kakao.com>
2021-06-16 17:03:10 +0900

    [Add] : FrontController 수정 및 클래스 틀 수정

--- src/controller/MyPortfolioController.java
+++ src/controller/MyPortfolioController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class MyPortfolioController implements Controller {
+    @Override
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response, String url) throws ServletException, IOException {
+        return null;
+    }
 }
--- src/controller/PortfolioBoardController.java
+++ src/controller/PortfolioBoardController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class PortfolioBoardController implements Controller {
+    @Override
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response, String url) throws ServletException, IOException {
+        return null;
+    }
+
 }
