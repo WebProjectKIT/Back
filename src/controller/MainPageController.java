5712bcf9fb4af272242b6ec2c5275c4fc1634398
jinoo7099 <vxc7099@gmail.com>
2021-06-16 20:16:44 +0900

    [Add] : main 페이지 포트폴리오 read 기능 추가, FrontController 수정

--- src/controller/MainPageController.java
+++ src/controller/MainPageController.java
@@ -1,13 +1,33 @@
 package controller;
 
+import domain.PortfolioBoard;
+import service.MainPageService;
+import service.PortfolioBoardService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainPageController implements Controller {
+
+    private final MainPageService mainPageService = new MainPageService();
+
     @Override
     public ModelAndView process(HttpServletRequest request, HttpServletResponse response, String url) throws ServletException, IOException {
-        return null;
+        ModelAndView modelAndView = new ModelAndView();
+
+        if(url.equals("/")) {
+            if(request.getMethod().equals("GET")) {
+                ArrayList<PortfolioBoard> boards = mainPageService.findBoards();
+//                ArrayList<PortfolioBoard>
+                modelAndView.setViewName("main");
+                modelAndView.getModel().put("boards", boards);
+            }
+        } else {
+            modelAndView.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        return modelAndView;
     }
 }
